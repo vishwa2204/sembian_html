6084eb8f0505955fc18f1e398b73cce9f5bd26a1
vishwa2204 <vishwa.m2204@gmail.com>
2025-03-20 22:27:57 +0530

    commit

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+// navbar 
+$(function () {
+var navMenu = document.getElementById("");
+
+$("#ham").on("click", ()=>{
+    $("#navMenus").toggleClass("serviceOpen");
+    $(".h-1").toggleClass("h-cross1");
+    $(".h-2").toggleClass("h-cross2");
+    $(".h-3").toggleClass("h-cross3");
+} )
+    
+})
